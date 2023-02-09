0b7f18dce43fcfc9f6fe038f2efa4c43db57b282
r4pidstart <r4pidstart@gmail.com>
2023-02-09 17:34:46 +0900

    module07 ex01 cannot handle const array

--- module07/ex01/iter.hpp
+++ module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: tjo <tjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 03:35:15 by tjo               #+#    #+#             */
-/*   Updated: 2023/02/08 04:11:50 by tjo              ###   ########.fr       */
+/*   Updated: 2023/02/09 17:34:12 by tjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@
 
 template<class T>
 void iter(T *arrptr, size_t len, void (*funcptr)(T&))
+{
+    for(size_t i=0; i<len; i++)
+        funcptr(arrptr[i]);
+}
+
+template<class T>
+void iter(const T *arrptr, size_t len, void (*funcptr)(const T&))
 {
     for(size_t i=0; i<len; i++)
         funcptr(arrptr[i]);
